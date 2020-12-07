141e3415fc13a382d3074e60c241c0c4e3c98579
Artur Jakubowski <a.jakubowskipl@gmail.com>
2020-12-07 11:47:44 +0100

    Initial commit

---
+++ src/main/java/App.java
@@ -0,0 +1,52 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Scanner;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        List<Integer> acceptedCoins = new ArrayList<>();
+        acceptedCoins.add(10);
+        acceptedCoins.add(20);
+        acceptedCoins.add(50);
+        acceptedCoins.add(100);
+        acceptedCoins.add(200);
+        acceptedCoins.add(500);
+
+        HashMap<String, Integer> machineCoinQuantity = new HashMap<>();
+        machineCoinQuantity.put("tens", 5);
+        machineCoinQuantity.put("twenties", 4);
+        machineCoinQuantity.put("fifties", 4);
+        machineCoinQuantity.put("one", 3);
+        machineCoinQuantity.put("two", 2);
+        machineCoinQuantity.put("five", 1);
+
+
+        HashMap<Integer, Integer> products = new HashMap<>();
+        products.put(1, 80);
+        products.put(2, 150);
+        products.put(3, 220);
+        products.put(4, 500);
+
+
+        Scanner scanner = new Scanner(System.in);
+        int product = scanner.nextInt();
+
+        int amount = 0;
+
+        System.out.println("1.Water 0.80  2.Sandwich 1.50  3.Chocolate 2.20  4.Chips 5");
+
+
+//        while (amount < products.get(product)){
+//
+//        }
+
+
+
+//        if (!acceptedCoins.contains(coins)){
+//            System.out.println("Wrong coin");
+//        }
+    }
+}
