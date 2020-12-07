77ef7587b672ec36eb6b714e117fa2beb1a173ab
Artur Jakubowski <a.jakubowskipl@gmail.com>
2020-12-07 14:53:39 +0100

    created change system. TODO list needs to be done

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -11,20 +11,20 @@ public class App {
         List<Integer> providedCoins = new ArrayList<>();
 
         List<Integer> acceptedCoins = new ArrayList<>();
+        acceptedCoins.add(1);
+        acceptedCoins.add(2);
+        acceptedCoins.add(5);
         acceptedCoins.add(10);
         acceptedCoins.add(20);
         acceptedCoins.add(50);
-        acceptedCoins.add(100);
-        acceptedCoins.add(200);
-        acceptedCoins.add(500);
 
-        HashMap<String, Integer> machineCoinQuantity = new HashMap<>();
-        machineCoinQuantity.put("tens", 5);
-        machineCoinQuantity.put("twenties", 4);
-        machineCoinQuantity.put("fifties", 4);
-        machineCoinQuantity.put("one", 3);
-        machineCoinQuantity.put("two", 2);
-        machineCoinQuantity.put("five", 1);
+        HashMap<Integer, Integer> machineCoinQuantity = new HashMap<>();
+        machineCoinQuantity.put(10, 5); // coin value, quantity
+        machineCoinQuantity.put(20, 4);
+        machineCoinQuantity.put(50, 4);
+        machineCoinQuantity.put(100, 3);
+        machineCoinQuantity.put(200, 2);
+        machineCoinQuantity.put(500, 1);
 
 
         HashMap<Integer, Integer> products = new HashMap<>();
@@ -52,8 +52,14 @@ public class App {
                 System.out.println("Wrong coin");
             }
 
+            if (coin <= 5 && coin != 0){ // makes coins 1, 2 and 5 a hundredths
+                coin *= 100;
+            }
+
             amount+= coin;
             providedCoins.add(coin);
+
+            machineCoinQuantity.put(coin, machineCoinQuantity.get(coin) + 1); // increments coin quantity by 1
         }
 
 
@@ -62,6 +68,40 @@ public class App {
         /*
         CHANGE SYSTEM
          */
+
+        /*
+        TODO
+        1. check if coin quantity is less than 3, if so make user input full price
+        2. refactor code
+         */
+
+        int productValue = products.get(product);
+        int userAmount = 0;
+
+        for (int coin : providedCoins){
+            userAmount+= coin;
+        }
+
+        int change = Math.abs(productValue - userAmount);
+        int chnageByCoin;
+
+        if(change == 0){
+            System.out.println("No change needed");
+        }else{
+
+            while(change != 0){
+                for (int acceptedCoin : acceptedCoins){
+                    acceptedCoin *= 10;
+
+                    if (change - acceptedCoin >= 0){
+                        chnageByCoin = acceptedCoin;
+                        change -= acceptedCoin;
+
+                        machineCoinQuantity.put(chnageByCoin, machineCoinQuantity.get(chnageByCoin) - 1);
+                    }
+                }
+            }
+        }
 
 
     }
