01d088670563e057d7ffef190a7fdc2df53228d6
Artur Jakubowski <a.jakubowskipl@gmail.com>
2020-12-08 11:45:02 +0100

    Payment system refactor

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,42 +8,77 @@ public class App {
     public static void main(String[] args) {
         System.out.println("1.Water 0.80  2.Sandwich 1.50  3.Chocolate 2.20  4.Chips 5");
 
-        List<Integer> providedCoins = new ArrayList<>();
-
-        List<Integer> acceptedCoins = new ArrayList<>();
-        acceptedCoins.add(1);
-        acceptedCoins.add(2);
-        acceptedCoins.add(5);
-        acceptedCoins.add(10);
-        acceptedCoins.add(20);
-        acceptedCoins.add(50);
-
-        HashMap<Integer, Integer> machineCoinQuantity = new HashMap<>();
-        machineCoinQuantity.put(10, 5); // coin value, quantity
-        machineCoinQuantity.put(20, 4);
-        machineCoinQuantity.put(50, 4);
-        machineCoinQuantity.put(100, 3);
-        machineCoinQuantity.put(200, 2);
-        machineCoinQuantity.put(500, 1);
-
+        int amount = 0;
+        int userAmount = 0;
 
         HashMap<Integer, Integer> products = new HashMap<>();
-        products.put(1, 80);
-        products.put(2, 150);
-        products.put(3, 220);
-        products.put(4, 500);
+        HashMap<Integer, Integer> machineCoinQuantity = new HashMap<>();
+
+        List<Integer> providedCoins = new ArrayList<>();
+        List<Integer> acceptedCoins = new ArrayList<>();
+
+        Scanner scanner = new Scanner(System.in);
 
 
+        fillTheArray(acceptedCoins);
+        fillTheHashMaps(machineCoinQuantity, products);
+
+
+//        boolean coinQuantityFlag = true;
+//        for (Integer value : machineCoinQuantity.values()){
+//            if (value >= 3){
+//                coinQuantityFlag = false;
+//                break;
+//            }
+//        }
+
+
+        int product = scanner.nextInt();
+        paymentSystem(amount, products, machineCoinQuantity, providedCoins, acceptedCoins, scanner, product);
 
 
         /*
-        COIN INPUT SYSTEM
+        CHANGE SYSTEM
          */
-        Scanner scanner = new Scanner(System.in);
-        int product = scanner.nextInt();
-        int amount = 0;
+
+        /*
+        TODO
+        1. check if coin quantity is less than 3, if so make user input full price
+        2. refactor code
+         */
 
 
+
+        int change = setChange(userAmount, products, providedCoins, product);
+        if(change == 0){
+            System.out.println("No change needed");
+        }else{
+
+            while(change != 0){
+                for (int acceptedCoin : acceptedCoins){
+                    acceptedCoin *= 10;
+
+                    if (change - acceptedCoin >= 0){
+                        change -= acceptedCoin;
+                        System.out.println(acceptedCoin);
+                        machineCoinQuantity.put(acceptedCoin, machineCoinQuantity.get(acceptedCoin) - 1);
+                    }
+                }
+            }
+        }
+
+
+    }
+
+    public static int setChange(int userAmount, HashMap<Integer, Integer> products, List<Integer> providedCoins, int product) {
+        int productValue = products.get(product);
+        for (int coin : providedCoins){
+            userAmount+= coin;
+        }
+        return Math.abs(productValue - userAmount);
+    }
+
+    public static void paymentSystem(int amount, HashMap<Integer, Integer> products, HashMap<Integer, Integer> machineCoinQuantity, List<Integer> providedCoins, List<Integer> acceptedCoins, Scanner scanner, int product) {
         while (amount < products.get(product)){
             int coin = scanner.nextInt();
 
@@ -61,48 +96,29 @@ public class App {
 
             machineCoinQuantity.put(coin, machineCoinQuantity.get(coin) + 1); // increments coin quantity by 1
         }
+    }
 
+    public static void fillTheArray(List<Integer> acceptedCoins) {
+        acceptedCoins.add(1);
+        acceptedCoins.add(2);
+        acceptedCoins.add(5);
+        acceptedCoins.add(10);
+        acceptedCoins.add(20);
+        acceptedCoins.add(50);
+    }
 
+    public static void fillTheHashMaps(HashMap<Integer, Integer> machineCoinQuantity, HashMap<Integer, Integer> products) {
+        machineCoinQuantity.put(0,0);
+        machineCoinQuantity.put(10, 5); // coin value, quantity
+        machineCoinQuantity.put(20, 4);
+        machineCoinQuantity.put(50, 4);
+        machineCoinQuantity.put(100, 3);
+        machineCoinQuantity.put(200, 2);
+        machineCoinQuantity.put(500, 1);
 
-
-        /*
-        CHANGE SYSTEM
-         */
-
-        /*
-        TODO
-        1. check if coin quantity is less than 3, if so make user input full price
-        2. refactor code
-         */
-
-        int productValue = products.get(product);
-        int userAmount = 0;
-
-        for (int coin : providedCoins){
-            userAmount+= coin;
-        }
-
-        int change = Math.abs(productValue - userAmount);
-        int chnageByCoin;
-
-        if(change == 0){
-            System.out.println("No change needed");
-        }else{
-
-            while(change != 0){
-                for (int acceptedCoin : acceptedCoins){
-                    acceptedCoin *= 10;
-
-                    if (change - acceptedCoin >= 0){
-                        chnageByCoin = acceptedCoin;
-                        change -= acceptedCoin;
-
-                        machineCoinQuantity.put(chnageByCoin, machineCoinQuantity.get(chnageByCoin) - 1);
-                    }
-                }
-            }
-        }
-
-
+        products.put(1, 80);
+        products.put(2, 150);
+        products.put(3, 220);
+        products.put(4, 500);
     }
 }
